9000be481bdde46bce1948210f827e29beefd577
Brett <benderb73@gmail.com>
2018-12-13 21:17:08 -0500

    ay

--- app.js
+++ app.js
@@ -8,12 +8,14 @@ var config = {
   };
   firebase.initializeApp(config);
 
+  /*arbitrary code to see if connected to firebase
   var database = firebase.database();
   var ref = database.ref("scores");
   var data = {
       name: "BLB",
       score: 43
   }
+  */
   ref.push(data);
 
  console.log(firebase);
@@ -24,10 +26,10 @@ var config = {
  var frequency= 0;
 
  //Grabbed values from text boxes
-//  name = $("#trainName").val().trim();
-//  destination = $("#trainDestination").val().trim();
-//  time = $("#firstTrainTime").val().trim();
-//  frequency = $("#frequency").val().trim();
+  name = $("#trainName").val().trim();
+  destination = $("#trainDestination").val().trim();
+  time = $("#firstTrainTime").val().trim();
+  frequency = $("#frequency").val().trim();
 
  $("#submit").click( function() {
     name = $("#trainName").val().trim();
@@ -43,34 +45,4 @@ var config = {
      )
  });
 
- // Assumptions
- var tFrequency = 3;
-
- // Time is 3:30 AM
- var firstTime = "03:30";
-
- // First Time (pushed back 1 year to make sure it comes before current time)
- var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
- console.log(firstTimeConverted);
-
- // Current Time
- var currentTime = moment();
- console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
-
- // Difference between the times
- var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
- console.log("DIFFERENCE IN TIME: " + diffTime);
-
- // Time apart (remainder)
- var tRemainder = diffTime % tFrequency;
- console.log(tRemainder);
-
- // Minute Until Train
- var tMinutesTillTrain = tFrequency - tRemainder;
- console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
-
- // Next Train
- var nextTrain = moment().add(tMinutesTillTrain, "minutes");
- console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
-
-
+ 
